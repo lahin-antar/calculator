6953e4057f7489da23d4c5524dcc2bd4e6ed6c85
Nihal <ratna.nihal.kunala@sap.com>
2022-06-11 16:45:59 +0530

    code for two numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,6 +6,11 @@ public class Calculator {
         if (isBlank(numbers)) {
             return 0;
         }
-        return Integer.parseInt(numbers);
+        String nums[] = numbers.split(",");
+        if (nums.length == 1) {
+            return Integer.parseInt(nums[0]);
+        } else {
+            return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+        }
     }
 }
