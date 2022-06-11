48659d8f54e4298b0208d9c25282cc9f4e4652d8
Nihal <ratna.nihal.kunala@sap.com>
2022-06-11 16:23:53 +0530

    test for two numbers

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,4 +13,9 @@ public class CalculatorTest {
     public void testForOneNumber() {
         assertEquals(1, Calculator.add("1"));
     }
+
+    @Test
+    public void testForTwoNumbers() {
+        assertEquals(7, Calculator.add("3,4"));
+    }
 }
